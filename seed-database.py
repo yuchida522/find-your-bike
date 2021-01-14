e6f5f225fb1483bc7d0829e91fd18618010026b0
KelsiAnderson <kelsi.anderson26@gmail.com>
2021-01-14 16:05:11 +0000

    added create user and accessory function to crud and seed

--- seed-database.py
+++ seed-database.py
@@ -9,4 +9,23 @@ from server import app
 connect_to_db(app)
 db.create_all()
 
+def open_pipe_file():
+    with(pipe_file) as open_file:
+        result_list = []
+
+        for line in open_file:
+            result_list.append(line.rstrip().split('|'))
+
+        return result 
+
+user_file = open_pipe_file("test_data/user.csv")
+for user in user_file:
+    fname, lname, email, phone_num, created_at, location_id = user
+    all_user = crud.create_user(fname, lname, email phone_num, created_at, location_id)
+
+accessory_file = open_pipe_file("test_data/accessories.csv")
+for acc in accessory_file:
+    title, description, condition, price, status_id, listing_id  = acc
+    all_acc = crud.create_accessory(title, description, condition, price, status_id, listing_id)
+
 
