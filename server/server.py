01d523a4316727fd2bb5dc10804f159383b0c069
yuchida522 <yuchida522@gmail.com>
2021-01-26 21:15:13 -0800

    commented on host number

--- server/server.py
+++ server/server.py
@@ -15,4 +15,5 @@ def homepage():
 
 if __name__ == '__main__':
     connect_to_db(app)
+    # app.run(host='127.0.0.1', debug=True, port = 5002) omnly this host number works for Yuri, look into why
     app.run(host='0.0.0.0', debug=True, port = 5002)
