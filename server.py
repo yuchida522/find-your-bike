9fffd27ef9d76e8654bd99b8fa29714ccfc0989c
Enid-Sky <enid.sky@protonmail.com>
2021-01-27 16:31:34 -0800

    add navbar route to server file

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 """ Server for Online Bike Exchange """
+from model import connect_to_db, db
 from flask import Flask, flash, redirect, render_template, request, session
 from jinja2 import StrictUndefined
 """this will be there server file"""
 # import crud
-from model import connect_to_db, db
 
 app = Flask(__name__)
 app.secret_key = "online_bike_exchange"
@@ -12,7 +12,12 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def homepage():
-    
+
+    return render_template('base.html')
+
+
+@app.route('/navbar')
+def nav():
     return render_template('base.html')
 
 
@@ -29,6 +34,6 @@ def profile():
 if __name__ == '__main__':
     connect_to_db(app)
     # only this host number works for Yuri, look into why
-    app.run(host='127.0.0.1', debug=True, port = 5002) 
+    app.run(host='127.0.0.1', debug=True, port=5002)
 
     # app.run(host='0.0.0.0', debug=True, port = 5002)
