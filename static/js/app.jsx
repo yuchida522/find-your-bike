0560ab95affa3c779718d18d864b80ce27515ac9
Enid-Sky <enid.sky@protonmail.com>
2021-01-28 12:55:18 -0800

    add route for navbar

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -4,12 +4,18 @@ function App() {
 	return (
 		<Router>
 			<Switch>
+
 				<Route exact path='/login'>
 					<Login />
 				</Route>
 				<Route exact path='/profile'>
 					<Profile />
 				</Route>
+
+                <Route exact path='/navbar'>
+					<Login />
+				</Route>
+
 			</Switch>
 		</Router>	
 	);
