971183d325a9f9bd72053f517110443a2eb8dbb2
yuchida522 <yuchida522@gmail.com>
2021-02-16 10:52:49 -0800

    added upload image button

---
+++ server/static/js/createListing.jsx
@@ -0,0 +1,25 @@
+'use strict';
+
+
+function CreateListing () {
+	const [uploadImageState, setUploadImageState] = React.useState('');
+	const [selectedFile, setSelectedFile] = React.useState('');
+	
+	const handleUploadImage = (e) => {
+		const image = e.target.files[0];
+	}
+	//functions will go here
+	return (
+    <React.Fragment>
+			<h1>Create Listing</h1>
+			<Form>
+			<Form.Group>
+    		<Form.File id="imageUpload" label="Upload Image"
+				 onChange={handleUploadImage} value={uploadImageState}
+				className="photo-upload"  />
+  		</Form.Group>
+
+			</Form>
+    </React.Fragment>
+	);
+}
