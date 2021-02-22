55f7ef375483e2610732ddea462328c7c67ea4bf
yuchida522 <yuchida522@gmail.com>
2021-02-22 10:49:51 -0800

    Add states to createListing component

--- server/static/js/createListing.jsx
+++ server/static/js/createListing.jsx
@@ -3,15 +3,38 @@
 
 function CreateListing () {
 	// const [uploadImageState, setUploadImageState] = React.useState('');
-	const [selectedFile, setSelectedFile] = React.useState('');
-	const myWidget = cloudinary.createUploadWidget({cloudName:'onlinebike', upload_preset:'g3w96a71',}, 
+	const [image, setImage] = React.useState('');
+	const [title, setTitle] = React.useState('');
+	const [description, setDescription] = React.useState('');
+	const [conditionNew, setConditionNew] = React.useState(false);
+	const [conditionUsed, setConditionUsed] = React.useState(false);
+	const [price, setPrice] = React.useState('');
+	const [bikeType, setBikeType] = React.useState('');
+
+
+	const myWidget = cloudinary.createUploadWidget({cloudName:'online-bike-exchange', upload_preset:'g3w96a71',}, 
 						(error, result) => {if (result.event == "success") {
-							setSelectedFile(result.info.url)
-						}})
-	// const handleUploadImage = (e) => {
-	// 	const image = e.target.files[0];
-	// }
-	//functions will go here
+							setImage(result.info.url)
+						}});
+	
+	const checkedBoxes = new Set()
+	
+	function handleTitleChange(evt) {
+		setTitle(evt.target.value)
+	};
+
+	function handleDescriptionChange(evt) {
+		setDescription(evt.target.value)
+	};
+
+	function handlePriceChange(evt) {
+		setPrice(evt.target.value)
+	}
+
+	function handleBikeTypeChange(evt) {
+		setBikeType(evt.target.value)
+	}
+	
 	return (
     <React.Fragment>
 			<h1>Create Listing</h1>
@@ -21,7 +44,7 @@ function CreateListing () {
 						Title
 					</Form.Label>
 					<Col sm="10">
-						<Form.Control type="text" placeholder="Title" />
+						<Form.Control type="text" placeholder="Title" value={title} onChange={handleTitleChange}/>
 					</Col>
   			</Form.Group>
 				<Form.Group as={Row} controlId="listingPhotos">
@@ -29,6 +52,7 @@ function CreateListing () {
 						Images
 					</Form.Label>
 					<Col sm="10">
+						<Image id="upload-image" src={image}></Image>
 						<Button id="upload-image-btn" onClick={()=> {myWidget.open()}}>Upload Images</Button>
 					</Col>
 				</Form.Group>
@@ -37,30 +61,30 @@ function CreateListing () {
 						Description
 					</Form.Label>
 					<Col sm="10">
-						<Form.Control as="textarea" rows={3} placeholder="Please give us a detailed description of your bike" />
+						<Form.Control as="textarea" rows={3} placeholder="Description" value={description} onChange={handleDescriptionChange}/>
 					</Col>
   			</Form.Group>
 				<Form.Group as={Row} controlId="listingCondition">
 					<Form.Label column sm="2">
 						Condition
 					</Form.Label>
-						<Form.Check inline label="New" type="checkbox" />
-						<Form.Check inline label="Used" type="checkbox" />
+						<Form.Check inline label="New" value="new"  id="new-condition" type="checkbox" />
+						<Form.Check inline label="Used" value="used" id="used-condition" type="checkbox" />
   			</Form.Group>
 				<Form.Group as={Row} controlId="listingType">
 					<Form.Label column sm="2">
 						Listing Type
 					</Form.Label>
-						<Form.Check inline label="For sale" type="checkbox" />
-						<Form.Check inline label="For trade" type="checkbox" />
-						<Form.Check inline label="For free" type="checkbox" />
+						<Form.Check inline label="For sale" value="sale" type="checkbox" />
+						<Form.Check inline label="For trade" value="trade" type="checkbox" />
+						<Form.Check inline label="For free" value="free" type="checkbox" />
   			</Form.Group>
 				<Form.Group as={Row} controlId="listingPrice">
 					<Form.Label column sm="2">
 						Price
 					</Form.Label>
 					<Col sm="7">
-						<Form.Control type="text" placeholder="required" />
+						<Form.Control type="text" placeholder="required" value={price} onChange={handlePriceChange}/>
 					</Col>
   			</Form.Group>
 					<Row>
@@ -68,7 +92,7 @@ function CreateListing () {
 							Bike Type
 						</Form.Label>
 						<Col sm="3">
-							<Form.Control type="text" placeholder="required" />
+							<Form.Control type="text" placeholder="required" value={bikeType} onChange={handleBikeTypeChange}/>
 						</Col>
 						<Form.Label column sm="2">
 							Model
